57e7497b250ef20fe187be8cf76e4aef32538083
Thiago Cavalcante <thiagoacavalcante_@hotmail.com>
2025-07-02 13:14:53 -0300

    Exercicio 30-2

--- configs/hooks.conf.js
+++ configs/hooks.conf.js
@@ -1,21 +1,26 @@
 let hooksConf = {
     afterStep: function (test, scenario, { error, duration, passed }) {
-        if(error) {
-            driver.takeScreenshot()
+        if (error) {
+            driver.takeScreenshot();
         }
     },
-    beforeSuite: async function(){
-        //app ja esta instalado e executando
-        let state = await driver.queryAppState("br.art.ebaconline")
-        if(state !== 4){
-            await driver.launchApp()
-        }
-    },
-    afterSuite: async function(){
-        //fechar o app
-        await driver.closeApp()
-    }
-}
-    
 
-module.exports = {hooksConf}
+    beforeSuite: async function () {
+        // Verifica se o app está rodando. Se não estiver, ativa o app
+        let state = await driver.queryAppState("br.art.ebaconline");
+        if (state !== 4) {
+            await driver.execute("mobile: activateApp", {
+                appId: "br.art.ebaconline"
+            });
+        }
+    },
+
+    afterSuite: async function () {
+        // Fecha o app após a suíte de testes
+        await driver.execute("mobile: terminateApp", {
+            appId: "br.art.ebaconline"
+        });
+    }
+};
+
+module.exports = { hooksConf };
